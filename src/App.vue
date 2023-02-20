76fae69df12eb9b456aa972fe57b56235fe25cd7
Zerio <54480523+Z3rio@users.noreply.github.com>
2023-02-20 16:32:28 +0100

    Fix centering

--- src/App.vue
+++ src/App.vue
@@ -53,8 +53,8 @@ const stats: Ref<Stat[]> = ref([
   height: 512px;
 
   position: absolute;
-  top: 50%;
-  left: 50%;
+  top: 50vh;
+  left: 50vw;
   transform: translate(-50%, -50%);
 
   display: flex;
